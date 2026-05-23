ff4acc5a53d6116eaca57fa708823a39e35a7636
engs-alexsandrosouza-dev <engs-alexsandrosouza@camporeal.edu.br>
2026-05-23 19:30:15 -0300

    Mapa com range;
    Gera uma "mapa" com valor aleatório, da pra ter uma noção de como é ksksks

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 using namespace std;
 
-int main (){
-    
+
+int main() {
+	int pos [15][15];
+
+	for (int i = 0; i <= 14; i++) {
+
+		for (int j = 0; j <= 14; j++) {
+
+			pos [i][j] = rand () % 10;
+			cout << pos [i][j] << " ";
+		}
+
+	}
 }
