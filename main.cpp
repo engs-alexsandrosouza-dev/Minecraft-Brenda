6dc59d36da2bcd7228af6d8f3ef544ebeb2bf79e
Lucas Borille <engs-lucasborille@camporeal.edu.br>
2026-05-28 22:41:39 -0300

    a

--- main.cpp
+++ main.cpp
@@ -1,14 +1,9 @@
 #include <iostream>
-
+#include <cstdlib>
 using namespace std;
 
+
 int main() {
-    int gravetos = 0;
-    int pedras = 0;
-    int frutas = 0;
-    int animais = 0;
-    int arvores = 0;
-    char mapa [16][16];
-
+	cout << "Hello, World!" << endl;
+	return 0;
 }
-
