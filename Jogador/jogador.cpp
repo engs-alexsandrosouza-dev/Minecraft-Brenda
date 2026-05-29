552ee8a4eb5ffb46431ccb1b7a2be9240163c710
Lucas Borille <engs-lucasborille@camporeal.edu.br>
2026-05-29 00:38:47 -0300

    jogador

---
+++ Jogador/jogador.cpp
@@ -0,0 +1,19 @@
+#include "jogador.hpp"
+#include <cctype>
+
+namespace jogador {
+
+void mover(char comando, int &linha, int &coluna) {
+    comando = static_cast<char>(std::tolower(static_cast<unsigned char>(comando)));
+    if (comando == 'w') {
+        --linha;
+    } else if (comando == 's') {
+        ++linha;
+    } else if (comando == 'a') {
+        --coluna;
+    } else if (comando == 'd') {
+        ++coluna;
+    }
+}
+
+} 
