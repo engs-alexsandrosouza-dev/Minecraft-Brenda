b37d6e61c467d244d8b3c3b402e9cca51063667d
engs-alexsandrosouza-dev <engs-alexsandrosouza@camporeal.edu.br>
2026-05-28 21:07:59 -0300

    mapa

--- mapa.cpp
+++ mapa.cpp
@@ -3,16 +3,38 @@
 using namespace std;
 
 
-int main() {
-	int pos [15][15];
+void mapa() {
+	char  pos [15][15];
 
 	for (int i = 0; i <= 14; i++) {
-
 		for (int j = 0; j <= 14; j++) {
-
-			pos [i][j] = rand () % 10;
-			cout << pos [i][j] << " " ;
+			pos [i][j] = 'x';
 		}
-        cout << endl;
 	}
+
+	for (int i = 0; i <= 5; i++) {
+		for (int j = 0; j <= 5; j++) {
+			int arvore = rand () % 15;
+			int tronco = rand ()% 15;
+			pos[arvore][tronco] = 'A';
+		}
+	}
+
+	for (int i = 0; i <= 3; i++) {
+		for (int j = 0; j <= 3; j++) {
+			int pedra = rand () % 3;
+			int rocha = rand ()% 13;
+			pos[pedra][rocha] = 'P';
+		}
 }
+		for (int i = 0; i <= 14; i++) {
+			for (int j = 0; j <= 14; j++) {
+			    cout << pos [i][j];
+			}
+		cout << endl;
+		}
+}
+		int main() {
+
+			mapa();
+		}
