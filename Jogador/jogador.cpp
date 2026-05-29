342bd3e70904ea447769a75a0fc4667feae1ba18
Lucas Borille <engs-lucasborille@camporeal.edu.br>
2026-05-28 23:47:48 -0300

    Criando Jogador

---
+++ Jogador/jogador.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void mover() {
+    if jogador == 'w' {
+        cout << "Voce se moveu para cima" << endl;
+    }
+    else if jogador == 's' {
+        cout << "Voce se moveu para baixo" << endl;
+    }
+    else if jogador == 'a' {
+        cout << "Voce se moveu para esquerda" << endl;
+    }
+    else if jogador == 'd' {
+        cout << "Voce se moveu para direita" << endl;
+    }
+}
