e0a59e686c784ef9b98c062cd4e2e065c746178c
engs-alexsandrosouza-dev <engs-alexsandrosouza@camporeal.edu.br>
2026-06-03 20:19:17 -0300

    lalala

--- alex.cpp
+++ alex.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdlib>
+#include <ctime>
 
 using namespace std;
 int main() {
@@ -12,43 +13,53 @@ int main() {
 		}       // chave for 1
 	}       // cheve for 2
 
+	for (int i = 0; i < 6; i++) {
+		for (int j = 0; j < 6; j++) {
+			if (pos [i][j] == 'x') {
+				int arvore = rand() % 16;             //SORTEIO DAS ARVORES
+				int tronco = rand()% 16;
+				pos[arvore][tronco] = 'T';
+			}
+		}       // chave for 1
+	}       // cheve for 2
+
 	for (int i = 0; i < 5; i++) {
 		for (int j = 0; j < 5; j++) {
-			int arvore = rand() % 16;             //SORTEIO DAS ARVORES
-			int tronco = rand()% 16;
-			pos[arvore][tronco] = 'T';
+			if (pos [i][j] == 'x') {
+				int fruta =  rand() % 16;
+				int banana = rand()% 16;
+				pos[fruta][banana] = 'F';
+			}
 		}       // chave for 1
 	}       // cheve for 2
 
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 3; j++) {
-			int fruta = rand() % 16;
-			int banana = rand()% 16;
-			pos[fruta][banana] = 'F';
-		}       // chave for 1
-	}       // cheve for 2
-
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 3; j++) {
-			int pedra = rand() % 16;          //SORTEIO DAS PEDRAS
-			int rocha = rand()% 16;
-			pos[pedra][rocha] = 'P';
+	for (int i = 0; i < 6; i++) {
+		for (int j = 0; j < 6; j++) {
+			if (pos [i][j] == 'x') {
+				int pedra = rand() % 16;          //SORTEIO DAS PEDRAS
+				int rocha = rand()% 16;
+				pos[pedra][rocha] = 'P';
+			}
 		}  // chave for 1
 	}       // cheve for 2
 
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 3; j++) {
-			int animal = rand() % 16;             //SORTEIO DOS ANIMAIS
-			int bicho = rand()% 16;
-			pos[animal][bicho] = 'A';
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 5; j++) {
+			if (pos [i][j] == 'x') {
+				int animal = rand() % 16;             //SORTEIO DOS ANIMAIS
+				int bicho = rand()% 16;
+				pos[animal][bicho] = 'A';
+			}
 		}    // chave for 1
 	}       // cheve for 2
 
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 3; j++) {
-			int graveto = rand() % 16;
-			int stick = rand()% 16;           // SORTEIO DOS GRAVETOS
-			pos[graveto][stick] = 'G';
+	for (int i = 0; i < 6; i++) {
+		for (int j = 0; j < 6; j++) {
+			if (pos [i][j] == 'x') {
+				int graveto = rand() % 16;
+				int galho = rand()% 16;           // SORTEIO DOS GRAVETOS
+				pos[graveto][galho] = 'G';
+			}
 		} // chave for 1
 	}   // cheve for 2
 
@@ -122,50 +133,88 @@ int main() {
 		cout << "Opção inválida! ";    // VERIFICAÇÃO
 		return 0;
 	} // chave if verificação
-	
-	
+
+
 	do {
 
 
-	for (int i = 0; i <= 15; i++) {
-		for (int j = 0; j <= 15; j++) {   //MAPA
-			cout << "  "  << pos [i][j];
-		} //cheve for 2
-		cout << endl;
-	} // chave for 1
+		for (int i = 0; i <= 15; i++) {
+			for (int j = 0; j <= 15; j++) {   //MAPA
+				cout << "  "  << pos [i][j];
+			} //cheve for 2
+			cout << endl;
+		} // chave for 1
+    
+        char coleta;
+
+		cout << "\n------------------------------------------------------------------------------------" << endl;
+		cout << "                                Inventário " << endl;
+		cout << "\n | Arvores: " << arvores << "         |";
+		cout << "\n | Animais: " << animais << "         |";
+		cout << "\n | Frutas: " << frutas << "          |";            //INVENTÁRIO
+		cout << "\n | Gravetos: " <<gravetos << "        |";
+		cout << "\n | Pedras: " << pedras << "          |";
+
+		cout << "\n O status da missão é: " << status << endl;
+
+		cout << "\n Faça um movimento: [w (frente) s (atrás)  a (esquerda) d (direita) ";
+		cin >> movimento;
+
+		pos [user][gamer] = {'x'};      // APAGA A POSIÇÃO ANTERIOR
+
+		switch (movimento) {
+		case 'w':
+			user -=1;
+			break;
+		case 'W':
+			user -= 1;
+			break;
+		case 's':
+			user += 1;
+			break;
+		case 'S':
+			user += 1;
+			break;                              //FAZ AS VARIÁVEIS DE LINHA E COLUNA SE ATUALIZARES
+		case 'a':
+			gamer -= 1;
+			break;
+		case 'A':
+			gamer -= 1;
+			break;
+		case 'd':
+			gamer += 1;
+			break;
+		case 'D':
+			gamer += 1;
+			break;
+		} // FECHA SWITCH MOVIMENTO
+
+		if (user > 15) {
+			user = 15;
+		}
+		if (user < 0) {
+			user = 0;   //NÃO DEIXA O USER SE MOVIMENTAR FORA DO MAPA
+		}
+		if (gamer > 15) {
+			gamer = 15;
+		}
+		if (gamer < 0) {
+			gamer = 0;
+		}
+		
+        	if (pos [user][gamer] != 'x'){
+		    cout << "Você quer coletar o intem: " << pos[user][gamer] << " [s ou n] ";
+		    cin >> coleta;
+		        if (coleta == 's' && coleta == 'S'){
+		            if (pos [user][gamer] == 'G'){gavetosnec += 1;gavetos += 1;}
+		        }
+		    }
+		}
+		pos [user][gamer] = {'@'};          //REESCREVE O ICONE @ NA NOVA POSIÇÃO DO USER
+
+	} while (status != 's' && status != 'S');       //CONDIÇÃO
 
 
-	cout << "\n------------------------------------------------------------------------------------" << endl;
-	cout << "                                Inventário " << endl;
-	cout << "\n | Arvores: " << arvores << "         |";
-	cout << "\n | Animais: " << animais << "         |";
-	cout << "\n | Frutas: " << frutas << "          |";            //INVENTÁRIO
-	cout << "\n | Gravetos: " <<gravetos << "        |";
-	cout << "\n | Pedras: " << pedras << "          |";
 
-	cout << "\n O status da missão é: " << status << endl;
-	
-	cout << "\n Faça um movimento: [w (frente) s (atrás)  a (esquerda) d (direita) ";
-	cin >> movimento;
-	
-	pos [user][gamer] = {'x'};      // APAGA A POSIÇÃO ANTERIOR
-	
-	switch (movimento){
-	    case 'w': user -=1; break;
-	    case 's': user += 1; break;             //FAZ AS VARIÁVEIS DE LINHA E COLUNA SE ATUALIZARES
-	    case 'a': gamer -= 1; break;
-	    case 'd': gamer += 1; break;
-	}
-	
-	if (user > 15){user = 15}
-	if (user < 0){user = 0}             //NÃO DEIXA O USER SE MOVIMENTAR FORA DO MAPA
-	if (gamer > 15) {gamer = 15}
-	if (gamer < 0){gamer = 0}
-	
-	pos [user][gamer] = {'@'};          //REESCREVE O ICONE @ NA NOVA POSIÇÃO DO USER
-	
-} while (status != 's' && status != 'S');       //CONDIÇÃO
-	
-	
-	
 } // chave int main
+
