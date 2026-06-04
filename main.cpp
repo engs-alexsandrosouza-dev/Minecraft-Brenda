5d7ba86665b251cb4fa4b5842d56095b6831d2e1
engs-alexsandrosouza-dev <engs-alexsandrosouza@camporeal.edu.br>
2026-06-03 22:11:42 -0300

    Funcionando

--- main.cpp
+++ main.cpp
@@ -1,61 +1,81 @@
 #include <iostream>
 #include <cstdlib>
+#include <ctime>
 
 using namespace std;
-int main () {
+int main() {
 
 	char  pos [16][16];
 
 	for (int i = 0; i <= 15; i++) {
-		for (int j = 0; j <= 15; j++) {
+		for (int j = 0; j <= 15; j++) {         //PREENCHIMENTO DO MAPA
 			pos [i][j] = 'x';
-		}
-	}
+		}       // chave for 1
+	}       // cheve for 2
+
+	for (int i = 0; i < 6; i++) {
+		for (int j = 0; j < 6; j++) {
+			if (pos [i][j] == 'x') {
+				int arvore = rand() % 16;             //SORTEIO DAS ARVORES
+				int tronco = rand()% 16;
+				pos[arvore][tronco] = 'T';
+			}
+		}       // chave for 1
+	}       // cheve for 2
 
 	for (int i = 0; i < 5; i++) {
 		for (int j = 0; j < 5; j++) {
-			int arvore = rand () % 16;
-			int tronco = rand ()% 16;
-			pos[arvore][tronco] = 'T';
-		}
-	}
+			if (pos [i][j] == 'x') {
+				int fruta =  rand() % 16;
+				int banana = rand()% 16;
+				pos[fruta][banana] = 'F';
+			}
+		}       // chave for 1
+	}       // cheve for 2
 
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 3; j++) {
-			int fruta = rand () % 15;
-			int banana = rand ()% 15;
-			pos[fruta][banana] = 'F';
-		}
-	}
+	for (int i = 0; i < 6; i++) {
+		for (int j = 0; j < 6; j++) {
+			if (pos [i][j] == 'x') {
+				int pedra = rand() % 16;          //SORTEIO DAS PEDRAS
+				int rocha = rand()% 16;
+				pos[pedra][rocha] = 'P';
+			}
+		}  // chave for 1
+	}       // cheve for 2
 
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 3; j++) {
-			int pedra = rand () % 16;
-			int rocha = rand ()% 16;
-			pos[pedra][rocha] = 'P';
-		}
-	}
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 5; j++) {
+			if (pos [i][j] == 'x') {
+				int animal = rand() % 16;             //SORTEIO DOS ANIMAIS
+				int bicho = rand()% 16;
+				pos[animal][bicho] = 'A';
+			}
+		}    // chave for 1
+	}       // cheve for 2
 
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 3; j++) {
-			int animal = rand () % 16;
-			int bicho = rand ()% 16;
-			pos[animal][bicho] = 'A';
-		}
-	}
+	for (int i = 0; i < 6; i++) {
+		for (int j = 0; j < 6; j++) {
+			if (pos [i][j] == 'x') {
+				int graveto = rand() % 16;
+				int galho = rand()% 16;           // SORTEIO DOS GRAVETOS
+				pos[graveto][galho] = 'G';
+			}
+		} // chave for 1
+	}   // cheve for 2
 
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 3; j++) {
-			int graveto = rand () % 16;
-			int stick = rand ()% 16;
-			pos[graveto][stick] = 'G';
-		}
-	}
-	int user = rand () % 16;
-	int gamer = rand ()% 16;
+	int user = rand() % 16;     //SORTEIO DO PERSONAGEM
+	int gamer = rand()% 16;
 	pos[user][gamer] = '@';
 
-	int pedras = 0, arvores = 0, frutas = 0, animais = 0, gravetos = 0, missao;
+
+
+	int pedrasnec = 0, arvoresnec = 0, frutasnec = 0, animaisnec = 0, gravetosnec = 0, missao; //VARIÁVEIS
+	char movimento, coleta;
+	string status;
+	int pedras = 0, arvores = 0, frutas = 0, animais = 0, gravetos = 0;
+
+
+
 
 	cout << "------------------------------------------------------------------------" << endl;
 	cout << "               Seja bem-vindo ao Mundo Quadrado!" << endl;
@@ -67,43 +87,168 @@ int main () {
 
 	cout << "\n  Escolha com cuidado sua aventura no mundo quadrado! " << endl;
 	cout << " 1 - Luz e Calor   ";
-	cout << "2 - Comida    ";
+	cout << "2 - Comida    ";                                       //INICIO
 	cout << "3 - Construção" << endl;
-	cout << "   Escolha sua missão: [1   2   3] : " << endl;
+
+
+
+
+
+	cout << "\n   Escolha sua missão: [1   2   3] : " << endl;
 	cin >> missao;
 
-	if (missao == 1 || missao == 2 || missao ==3) {
+	switch (missao) {
+	case 1:
+		gravetosnec = 4;
+		pedrasnec = 2;
+		cout << "\n Na missão Luz e Calor seu objetivo é coletar: 4 gravetos e 2 pedras no mapa! " << endl;
+		if (pedrasnec != pedras && gravetosnec != gravetos){ status = "Em progresso"; } else { status = "Concluído!!";}
+		break;
 
-		cout << "\n------------------------------------------------------------------------------------" << endl;
+	case 2:
+		frutasnec = 1;
+		animaisnec = 3;
+		cout << "\n Na missão Comida seu objetivo é coletar: 1 fruta e 3 animais! " << endl;
+		if (frutasnec != frutas && animaisnec != animais){ status = "Em progresso"; } else { status = "Concluído!!";}
+		break;
 
-		if (missao == 1) {
-			cout << "\n Na missão Luz e Calor seu objetivo é coletar: 4 gravetos e 2 pedras no mapa! " << endl;
-		}
-		else if (missao == 2) {
-			cout << "\n Na missão Comida seu objetivo é coletar: 1 fruta e 3 animais! " << endl;
-		}
-		else if (missao == 3) {
-			cout << "\n Na missão contrução seu objetivo é coletar: 8 pedras e 4 árvores! " << endl;
-		}
+	case 3:
+		pedrasnec = 8;
+		arvoresnec = 4;
+		cout << "\n Na missão contrução seu objetivo é coletar: 8 pedras e 4 árvores! " << endl;
+				if (arvoresnec!= arvores && pedrasnec != pedras){ status = "Em progresso"; } else { status = "Concluído!!";}
+		break;
 
-		cout << "\n Sua posição atual é: " << user << "-" <<gamer << endl;
-		cout << "\n Para se mover user as teclas w [cima] d [direita] s [baixo] a [esquerda]";
-		cout << "\n";
+	} //chave switch case missão
 
-		cout << "\n------------------------------------------------------------------------------------" << endl;
+
+	cout << "\n Sua posição atual é: " << user << "-" <<gamer << endl;
+	cout << "\n Para se mover user as teclas w [cima] d [direita] s [baixo] a [esquerda]";
+	cout << "\n";
+
+	cout << "\n------------------------------------------------------------------------------------" << endl;
+
+
+
+
+
+	if(missao != 1 && missao != 2 && missao != 3) {
+		cout << "Opção inválida! ";    // VERIFICAÇÃO
+		return 0;
+	} // chave if verificação
+
+
+	do {
+
+
 		for (int i = 0; i <= 15; i++) {
 			for (int j = 0; j <= 15; j++) {
-				cout << "  "  << pos [i][j];
-			}
+				if(pos[i][j]=='x') {
+					cout << "  "  << ".";
+				}
+				else
+				{
+					cout << "  "  << pos [i][j];
+				}//MAPA
+
+			} //cheve for 2
 			cout << endl;
+		} // chave for 1
+
+		char coleta;
+
+		cout << "\n------------------------------------------------------------------------------------" << endl;
+		cout << "                                Inventário " << endl;
+		cout << "\n | Arvores: " << arvores << "         |";
+		cout << "\n | Animais: " << animais << "         |";
+		cout << "\n | Frutas: " << frutas << "          |";            //INVENTÁRIO
+		cout << "\n | Gravetos: " <<gravetos << "        |";
+		cout << "\n | Pedras: " << pedras << "          |";
+
+		cout  << "\n O status da missão é: " << status << endl;
+
+		cout << "\n Faça um movimento: w (cima) s (baixo)  a (esquerda) d (direita) ";
+		cin >> movimento;
+
+		pos [user][gamer] = {'x'};      // APAGA A POSIÇÃO ANTERIOR
+
+		switch (movimento) {
+		case 'w':
+			user -=1;
+			break;
+		case 'W':
+			user -= 1;
+			break;
+		case 's':
+			user += 1;
+			break;
+		case 'S':
+			user += 1;
+			break;                              //FAZ AS VARIÁVEIS DE LINHA E COLUNA SE ATUALIZARES
+		case 'a':
+			gamer -= 1;
+			break;
+		case 'A':
+			gamer -= 1;
+			break;
+		case 'd':
+			gamer += 1;
+			break;
+		case 'D':
+			gamer += 1;
+			break;
+		} // FECHA SWITCH MOVIMENTO
+
+		if (user > 15) {
+			user = 15;
 		}
-	}
-		else {
-			cout << "Opção inválida! ";
-			return 0;
-		}		
+		if (user < 0) {
+			user = 0;   //NÃO DEIXA O USER SE MOVIMENTAR FORA DO MAPA
+		}
+		if (gamer > 15) {
+			gamer = 15;
+		}
+		if (gamer < 0) {
+			gamer = 0;
+		}
 
-    cout << "\n------------------------------------------------------------------------------------" << endl;
-    cout << "                                Inventário ";
+		if (pos [user][gamer] != 'x') {
+		    cout << pos [user][gamer] << endl << endl;
+			cout << "Você quer coletar o item: " << pos[user][gamer] << " [s ou n] ";
+			cin >> coleta;
+			if (coleta == 's' || coleta == 'S') {
+			    
+				if (pos [user][gamer] == 'G') {
+					gravetos += 1;
+				}
+			else if (pos [user][gamer] == 'T') {
+				arvores += 1;
+			}
 
-	}
+			else if (pos [user][gamer] == 'P') {
+				pedras += 1;
+			}
+
+            else if (pos [user][gamer] == 'A'){
+		            animais += 1;
+		            }
+		    else if (pos [user][gamer] == 'F'){
+		            frutas = frutas + 1;
+		            }
+			}
+		} // chave if item
+
+		pos [user][gamer] = {'@'};          //REESCREVE O ICONE @ NA NOVA POSIÇÃO DO USER
+		
+	    if (pedrasnec != pedras && gravetosnec != gravetos){ status = "Em progresso"; } else { status = "Concluído!!";}
+	    if (frutasnec != frutas && animaisnec != animais){ status = "Em progresso"; } else { status = "Concluído!!";}
+	    if (arvoresnec!= arvores && pedrasnec != pedras){ status = "Em progresso"; } else { status = "Concluído!!";}
+
+	    
+	    
+
+	} while (status != "Concluído!!");       //CONDIÇÃO
+
+
+
+} // chave int main
